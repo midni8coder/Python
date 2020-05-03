6b03459ed18f4d40a4d9c67446270ccb9679d8ec
Midnight Coder <62158642+midni8coder@users.noreply.github.com>
2020-05-03 15:49:28 +0530

    Modified Blob class

--- Sentdex_Python/Intermediate/blob.py
+++ Sentdex_Python/Intermediate/blob.py
@@ -7,16 +7,13 @@ import random
 class Blob:
 
 	def __init__(self, color, x_boundary, y_boundary, size_range=(4, 8), move_range=(-1, 2)):
-
+		self.move_range = move_range
 		self.x_boundary = x_boundary
 		self.y_boundary = y_boundary
 		self.x = random.randrange(0, self.x_boundary)
 		self.y = random.randrange(0, self.y_boundary)
 		self.size = random.randrange(size_range[0], size_range[1])
 		self.color = color
-		self.move_range = move_range
-
-	
 
 	def move(self, checkboundaries= True):
 		self.move_x = random.randrange(self.move_range[0], self.move_range[1])
